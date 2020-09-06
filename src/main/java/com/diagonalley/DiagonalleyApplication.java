39e086b3648aacb0de3fe3c639740c60d5b35d9b
sngupta <sngupta@expediagroup.com>
2020-09-06 19:40:53 +0530

    initial commit working greeting controller

---
+++ src/main/java/com/diagonalley/DiagonalleyApplication.java
@@ -0,0 +1,13 @@
+package com.diagonalley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiagonalleyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiagonalleyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diagonalley/diagonalley/DiagonalleyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diagonalley.diagonalley;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiagonalleyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
