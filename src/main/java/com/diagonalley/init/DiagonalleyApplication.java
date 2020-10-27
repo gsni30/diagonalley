84d7725670567568d962fbadf1b0f16f9ecb70d9
gsni30 <snigdha142.gupta@gmail.com>
2020-10-27 19:31:21 +0530

    add and load configs

--- src/main/java/com/diagonalley/init/DiagonalleyApplication.java
+++ src/main/java/com/diagonalley/init/DiagonalleyApplication.java
@@ -3,7 +3,7 @@ package com.diagonalley.init;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.diagonalley"})
 public class DiagonalleyApplication {
 
 	public static void main(String[] args) {
